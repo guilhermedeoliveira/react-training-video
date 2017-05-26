51367c6c8fbff7dac7f7cf57b2056f0c0ea67caf
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-05-26 17:13:04 -0300

    Improve index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoPlayer from './components/video_player'
 
+const API_KEY = 'AIzaSyBZoXcjJSallHsUTCClXkB_5__n4UEvICg';
+
+// Create a new Component
+// This component should produce some HTML
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('reactJS');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => this.videoSearch(term), 300)
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoPlayer video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// Take this component generated HTML and put it on the page (in the DOM)
 ReactDOM.render(
-    <App />,
-    document.querySelector('.container'));
+  <App />,
+  document.querySelector('.container')
+)
